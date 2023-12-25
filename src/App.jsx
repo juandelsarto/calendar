673d4654486b40c1b2e21d3256d45bd0201450fc
Juan Cruz Del Sarto <jdelsarto@around.net>
2023-12-25 12:19:50 -0300

    feat: render Customizer in main screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { Box, Center, ChakraProvider, Flex, Heading } from "@chakra-ui/react";
 import Customizer from "./modules/Customizer";
 import "./styles/main.scss";
-import {
-  ChakraProvider,
-} from "@chakra-ui/react";
 
 function App() {
-
   return (
     <ChakraProvider>
-      <Customizer />
+      <Flex flexDir="column" borderWidth="1px" alignContent="center" w="100%" h="100vh" p={4}>
+        <Center>
+          <Heading as="h3" size="lg">
+            Calendario
+          </Heading>
+        </Center>
+        <Flex flex={1} gap={4} py={4} >
+          <Box p={4} flex={1 / 2}>
+            <Heading as="h5" size="sm" mb={4}>
+              Personalizar:
+            </Heading>
+            <Customizer />
+          </Box>
+        </Flex>
+      </Flex>
     </ChakraProvider>
   );
   // return <Calendar owner={OWNERS.FLOR} />;
