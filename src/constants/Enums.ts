c0e8c883749021548d47d307e1333e13876d6938
Juan Cruz Del Sarto <jdelsarto@around.net>
2023-12-25 23:46:10 -0300

    feat: Options and Enums

---
+++ src/constants/Enums.ts
@@ -0,0 +1,64 @@
+export const YEAR = 2024;
+
+export enum OPTIONS {
+  OWNER = "owner",
+  LANGUAGE = "language",
+  STARTING_DAY = "startingDay",
+  SIZE = "size",
+  BACKGROUND = "background",
+  PRIMARY_COLOR = "primaryColor",
+  SECONDARY_COLOR = "secondaryColor",
+  THIRD_COLOR = "thirdColor",
+}
+
+export enum OWNERS {
+  FAMILIA = "familia",
+  FITA = "fita",
+  GRISEL = "grisel",
+  GABY = "gaby",
+  FLOR = "flor",
+  CASA = "casa",
+}
+
+export enum MONTHS {
+  JANUARY = "january",
+  FEBRUARY = "february",
+  MARCH = "march",
+  APRIL = "april",
+  MAY = "may",
+  JUNE = "june",
+  JULY = "july",
+  AUGUST = "august",
+  SEPTEMBER = "september",
+  OCTOBER = "october",
+  NOVEMBER = "november",
+  DECEMBER = "december",
+}
+
+export enum DAYS {
+  MONDAY = "monday",
+  TUESDAY = "tuesday",
+  WEDNESDAY = "wednesday",
+  THURSDAY = "thursday",
+  FRIDAY = "friday",
+  SATURDAY = "saturday",
+  SUNDAY = "sunday",
+}
+
+export enum LANGUAGES {
+  ENGLISH = "english",
+  SPANISH = "spanish",
+}
+
+export enum SIZES {
+  A3 = "a3",
+  A4 = "a4",
+  A5 = "a5",
+  OFICIO = "oficio",
+  CARTA = "carta",
+}
+
+export enum BACKGROUND {
+  BALLONS = "ballons",
+  UNICORN = "unicorn",
+}
---
+++ src/modules/Customizer/Options.ts
@@ -0,0 +1,59 @@
+import Ballons from "../../assets/bgCumple.png";
+import Unicorn from "../../assets/bgUnicorn.png";
+import {
+  BACKGROUND,
+  DAYS,
+  LANGUAGES,
+  OWNERS,
+  SIZES,
+} from "../../constants/Enums";
+
+export const OWNER_OPTIONS = [
+  {
+    value: OWNERS.GRISEL,
+    label: "Grisel",
+  },
+  {
+    value: OWNERS.GABY,
+    label: "Gaby",
+  },
+  {
+    value: OWNERS.FITA,
+    label: "Fiamma",
+  },
+  {
+    value: OWNERS.FAMILIA,
+    label: "Familia",
+  },
+  {
+    value: OWNERS.CASA,
+    label: "Casa",
+  },
+  {
+    value: OWNERS.FLOR,
+    label: "Flor",
+  },
+];
+
+export const LANGUAGE_OPTIONS = [
+  { value: LANGUAGES.ENGLISH, label: "Inglés" },
+  { value: LANGUAGES.SPANISH, label: "Español" },
+];
+
+export const STARTING_DAY_OPTIONS = [
+  { value: DAYS.SUNDAY, label: "Domingo" },
+  { value: DAYS.MONDAY, label: "Lunes" },
+];
+
+export const SIZE_OPTIONS = [
+  { value: SIZES.A3, label: "A3" },
+  { value: SIZES.A4, label: "A4" },
+  { value: SIZES.A5, label: "A5" },
+  { value: SIZES.OFICIO, label: "Oficio" },
+  { value: SIZES.CARTA, label: "Carta" },
+];
+
+export const BACKGROUND_OPTIONS = [
+  { value: BACKGROUND.BALLONS, label: "Globos", img: Ballons },
+  { value: BACKGROUND.UNICORN, label: "Unicornio", img: Unicorn },
+];
